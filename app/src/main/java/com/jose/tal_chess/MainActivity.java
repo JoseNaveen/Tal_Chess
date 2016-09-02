e3c183a0803159ec1226e02d077631e0a81d8e0a
JoseNaveen <jose8naveen@gmail.com>
2016-09-02 23:45:52 +0530

    Signin activity added-2

--- app/src/main/java/com/jose/tal_chess/MainActivity.java
+++ app/src/main/java/com/jose/tal_chess/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin);
     }
     public void authenticateUser(View view) {
         EditText username = (EditText) findViewById(R.id.username);
